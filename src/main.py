a14e10893c46789618aa87beb533578e07c9ccca
Atarairit <tyron.obichie3@gmail.com>
2026-08-17 20:58:32 +0200

    src und main erstellt. main wird die streamlit app sein (listen wird verschoben)

---
+++ src/main.py
@@ -0,0 +1,22 @@
+# Streamlit app that makes it easy to deploy a file server
+import streamlit as st
+import socket
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def listen():
+    """
+    when pressing the 'listen' button in the streamlit application
+    """
+    st.write("Listening")
+    return
+
+
+
+
+def main():
+    listen()
+
+if __name__ == "__main__":
+    main()
